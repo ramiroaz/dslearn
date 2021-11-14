72425840a864cb5b47ed17398fd841db282b698a
Ramiro Arce <ramiroaz@gmail.com>
2021-11-14 12:59:23 -0300

    Course

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -12,3 +12,5 @@ INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 1);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 3);
+
+INSERT INTO tb_course (name, img_Uri, img_Gray_Uri) VALUES ('Bootcamp HTML', 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fpxhere.com%2Fen%2Fphoto%2F1443423&psig=AOvVaw3l918gu7wFQqFSXSW_0Vlr&ust=1636991612584000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCJDz-pGbmPQCFQAAAAAdAAAAABAD', 'https://w7.pngwing.com/pngs/989/932/png-transparent-computer-icons-course-learning-education-training-angle-text-rectangle.png');
