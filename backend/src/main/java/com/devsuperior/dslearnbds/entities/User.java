28b7f0b40bc77050191dca34d1ad890a0e8780fd
Ramiro Arce <ramiroaz@gmail.com>
2021-11-19 14:41:46 -0300

    Fix : remove user roles on constructor

--- backend/src/main/java/com/devsuperior/dslearnbds/entities/User.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/entities/User.java
@@ -44,15 +44,13 @@ public class User implements Serializable {
 	}
 	
 
-	public User(Long id, String name, String email, String password, Set<Role> roles,
-			List<Notification> notifications) {
+	public User(Long id, String name, String email, String password) {
 		super();
 		this.id = id;
 		this.name = name;
 		this.email = email;
 		this.password = password;
-		this.roles = roles;
-		this.notifications = notifications;
+
 	}
 
 	public Long getId() {
