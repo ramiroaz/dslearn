45907f4250d15049d491b7a8bf4a36324e606cd4
Ramiro Arce <ramiroaz@gmail.com>
2021-11-20 16:17:24 -0300

    Custom content for current user

--- backend/src/main/java/com/devsuperior/dslearnbds/repositories/NotificationRepository.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/repositories/NotificationRepository.java
@@ -1,9 +1,16 @@
 package com.devsuperior.dslearnbds.repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.devsuperior.dslearnbds.entities.Notification;
+import com.devsuperior.dslearnbds.entities.User;
 
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
 
+	//Busca paginada das notificações por usuário
+	//forma temporaria, incompleta
+	Page<Notification> findByUser(User user, Pageable pageable);
+	
 }
